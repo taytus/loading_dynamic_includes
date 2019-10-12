668b3d976aebcc66019aae0cda367b2f03713828
Roberto Inetti <copertus@gmail.com>
2019-10-11 23:40:44 -0500

    first commit

---
+++ resources/views/includes/links.blade.php
@@ -0,0 +1,7 @@
+<div class="links">
+
+    @if(isset($link_url) && isset($link_label))
+        <a href="{{$link_url}}">{{$link_label}}</a>
+    @endif
+
+</div>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <!-- Blade Directive-->
+        @include('includes.style',[
+                'background_color'=>'#fff'
+
+        ])
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    BATMAN
+                </div>
+
+                @include('includes.links',[
+                'link_label'=>'Demo',
+                'link_url'=>route('demo')
+                ])
+
+            </div>
+        </div>
+    </body>
+</html>
